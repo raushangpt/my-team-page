0a42fcc50c71a036d1037c4f38111a83a0f92e8d
raushangpt <rk880488@gmail.com>
2023-02-24 05:09:54 +0530

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      'pt-serif': ['PT Serif', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
